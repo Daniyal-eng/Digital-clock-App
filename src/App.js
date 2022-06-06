fa01769d247c7ebed7a7df3288a8f9debb0dbfd6
Daniyal-eng <daniyalanjum172@gmail.com>
2022-06-07 01:56:42 +0500

    first commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,36 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState } from "react";
 
-function App() {
+export default function App() {
+
+
+
+  let CurTime = new Date().toLocaleTimeString();
+  let CurDate = new Date().toLocaleDateString();
+  
+  
+
+  const [Time, setTime] = useState(CurTime);
+  
+  
+  const updatetime=()=>{
+    let CurUpdatedTime = new Date().toLocaleTimeString();
+    
+    setTime(CurUpdatedTime);
+  }
+
+  setInterval(updatetime,1000)
+
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
-}
+    <>
 
-export default App;
+   
+    <div className="MyDiv">
+    <h1>Welcome to the Digital Clock</h1>  
+      <h2>{Time}</h2>
+      <span>{CurDate}</span> 
+        
+    </div>
+    </>
+    );
+}
